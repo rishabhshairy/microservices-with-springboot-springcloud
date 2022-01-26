1f13bd65f8391eaaa6a64827263491802a7250a0
Rishabh Shairy <rishusinha8@gmail.com>
2022-01-26 13:24:09 +0530

    Initial Commit

---
+++ restful-web-services/src/main/java/com/shairy/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,14 @@
+package com.shairy.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Test Changes");
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+}
