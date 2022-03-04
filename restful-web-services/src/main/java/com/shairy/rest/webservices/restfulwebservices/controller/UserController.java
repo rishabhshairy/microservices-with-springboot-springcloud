86c7fecc0e1c117190a00b85f007c50531b443a4
Rishabh Shairy <rishusinha8@gmail.com>
2022-03-04 23:40:04 +0530

    User controller

---
+++ restful-web-services/src/main/java/com/shairy/rest/webservices/restfulwebservices/controller/UserController.java
@@ -0,0 +1,64 @@
+package com.shairy.rest.webservices.restfulwebservices.controller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.shairy.rest.webservices.restfulwebservices.beans.User;
+import com.shairy.rest.webservices.restfulwebservices.dao.UserDAO;
+import com.shairy.rest.webservices.restfulwebservices.exceptions.UserNotFoundException;
+
+@RestController
+public class UserController {
+
+	private Logger logger = org.slf4j.LoggerFactory.getLogger(UserController.class);
+
+	@Autowired
+	private UserDAO userDAO;
+
+	@GetMapping("/users")
+	public List<User> getAllUsers() {
+		return userDAO.findAll();
+	}
+
+	@GetMapping("/users/{id}")
+	public User findById(@PathVariable int id) {
+		Optional<User> user = userDAO.findOne(id);
+		logger.debug(user.toString());
+		if (user.isEmpty()) {
+			throw new UserNotFoundException("id = " + id);
+
+		}
+		return user.get();
+	}
+
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = userDAO.save(user);
+
+		// returning location of saved user
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
+	}
+
+	@DeleteMapping("/user/{id}")
+	public ResponseEntity<Object> deleteUser(@PathVariable int id){
+		User user = userDAO.deleteById(id);
+		if (user==null) {
+			throw new UserNotFoundException("id = "+id);
+		}
+		return ResponseEntity.ok(user);
+	}
+}
