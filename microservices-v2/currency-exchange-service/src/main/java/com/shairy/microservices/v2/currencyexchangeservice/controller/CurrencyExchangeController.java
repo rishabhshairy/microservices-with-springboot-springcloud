a75e4b4f79c426067cec99dcc0705161e42c4993
Rishabh Shairy <rishusinha8@gmail.com>
2024-03-02 10:26:33 +0530

    Docker Learning (#3)
    * Add Zipkin to gateway
    
    * Add zipkin to currency services
    
    * Move gateway and naming server inside v2 folder
    
    * Add docker compose to launch applications

--- microservices-v2/currency-exchange-service/src/main/java/com/shairy/microservices/v2/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ microservices-v2/currency-exchange-service/src/main/java/com/shairy/microservices/v2/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -5,6 +5,8 @@ package com.shairy.microservices.v2.currencyexchangeservice.controller;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,11 +27,15 @@ public class CurrencyExchangeController {
 	@Autowired
 	private Environment env;
 
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Autowired
 	private ExchangeService service;
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange exchangeValues(@PathVariable String from, @PathVariable String to) {
+
+		logger.info("Retrieve exchange values from {} to {}", from, to);
 		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to,
 				service.getCurrencyExchange(from, to).getConversionMultiple());
 		currencyExchange.setEnvironment(env.getProperty("local.server.port"));
